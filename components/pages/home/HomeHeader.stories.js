b02636735929b004df9c6647d62feaddb7aaa974
Dipras <dipras75@gmail.com>
2022-01-20 20:10:06 +0700

    feat: home page header

---
+++ components/pages/home/HomeHeader.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomeHeader from "./HomeHeader";
+
+const defExport = {
+  title: "Pages/Home/Header",
+  component: HomeHeader,
+};
+
+export default defExport;
+const Template = (args) => <HomeHeader />;
+export const Header = Template.bind();
