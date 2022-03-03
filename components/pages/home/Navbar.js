78bdaa4c3c7ad2af6cbcd58dbf8737f14721850f
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-03-03 21:02:10 +0700

    fix: susunan import

--- components/pages/home/Navbar.js
+++ components/pages/home/Navbar.js
@@ -1,7 +1,7 @@
+import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { Icon } from "@iconify/react";
-import Link from "next/link";
-import { useState } from "react";
 
 const Navbar = () => {
   const [isDarkMode, setisDarkMode] = useState(false);
