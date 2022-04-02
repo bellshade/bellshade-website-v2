64f3ac40e865b0bb9d71f06af9c475660ca33cd1
Feri Irawan <Feriirawans1998@gmail.com>
2022-04-02 07:32:57 +0700

    feat: add discord section

---
+++ components/pages/home/Discord.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Discord from './Discord'
+
+const defExport = {
+  title: 'Pages/Home/Discord',
+  component: Discord,
+}
+
+export default defExport
+const Template = (args) => <Discord />
+export const Discord = Template.bind()
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Head from "next/head";
-import Navbar from "@/components/pages/home/Navbar";
-import HomeHeader from "@/components/pages/home/HomeHeader";
-import OurTeam from "@/components/pages/home/OurTeam";
+import Head from 'next/head'
+import Navbar from '@/components/pages/home/Navbar'
+import HomeHeader from '@/components/pages/home/HomeHeader'
+import Discord from '@/components/pages/home/Discord'
+import OurTeam from '@/components/pages/home/OurTeam'
 
-const Home = ({ members }) => {
+const Home = ({members}) => {
   return (
     <div>
       <Head>
@@ -11,23 +12,24 @@ const Home = ({ members }) => {
       </Head>
       <Navbar />
       <HomeHeader />
+      <Discord />
       <OurTeam members={members} />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
 
 export async function getStaticProps() {
-  const BellshadeAPI = "https://api.bellshade.org/";
-  const getContents = (url) => fetch(url).then((res) => res.json());
+  const BellshadeAPI = 'https://api.bellshade.org/'
+  const getContents = (url) => fetch(url).then((res) => res.json())
 
-  const getPublicMembers = await getContents(BellshadeAPI);
+  const getPublicMembers = await getContents(BellshadeAPI)
 
   return {
     props: {
       members: getPublicMembers,
     },
     revalidate: 432000,
-  };
+  }
 }
